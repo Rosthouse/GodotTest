206bc762db76d04a33b5692967bbaeb2b76c78e9
Patrick Joos <patrick.joos@ntb.ch>
2019-09-30 21:43:07 +0200

    Change: Implemented seeking behaviour correctly

--- Scenes/Seeker.gd
+++ Scenes/Seeker.gd
@@ -13,10 +13,15 @@ var velocity: Vector2 = Vector2.ZERO
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var steering_direction: Vector2 = Vector2(cos(self.rotation), sin(self.rotation))
-	var steering_force: Vector2 = steering_direction.clamped(max_force)
+	
+	#var steering_force: Vector2 = steering_direction.clamped(max_force)
+	var steering_force: Vector2 = self.get_steering().clamped(max_force)
 	var acceleration: Vector2 = steering_force / mass
 	
 	self.velocity = (velocity + acceleration).clamped(max_speed)
 	self.position = self.position + self.velocity
 	self.rotation = atan2(steering_direction.y, steering_direction.x)
 	
+func get_steering() -> Vector2:
+	var desired_velocity: Vector2 =  (self.target.position - self.position ).normalized() * self.max_speed
+	return desired_velocity - self.velocity
--- Scenes/SeekyBoi.gd
+++ Scenes/SeekyBoi.gd
@@ -7,5 +7,5 @@ onready var target: Node2D = get_node(target_node)
 func _input(event):
    # Mouse in viewport coordinates
 	if event.is_action_released("target_set"):
-		print("Mouse Click/Unclick at: ", event.position)
+		print("Set target to: ", event.position)
 		target.position = event.position
